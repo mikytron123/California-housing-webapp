233b19f6af159144e2e3aa6d062a3e9c6d434cde
Miky <vyasmiky@gmail.com>
2023-08-31 17:48:58 -0400

    formatted using black

--- api/src/bento_save.py
+++ api/src/bento_save.py
@@ -3,5 +3,5 @@ import bentoml
 
 model = joblib.load("model.pkl")
 
-saved_model = bentoml.sklearn.save_model("SVR",model)
+saved_model = bentoml.sklearn.save_model("SVR", model)
 print(f"Model saved: {saved_model}")
--- streamlit/app.py
+++ streamlit/app.py
@@ -1,27 +1,26 @@
-
 import streamlit as st
 import pandas as pd
 import requests
+
+
 def main():
-    #headers
-    st.title('California Housing Model')
-    st.write('This app enables users to make predictions using a scikit learn model')
-    #data upload
-    st.subheader('1. Data upload')
-    st.write('Upload a csv for predictions')
+    # headers
+    st.title("California Housing Model")
+    st.write("This app enables users to make predictions using a scikit learn model")
+    # data upload
+    st.subheader("1. Data upload")
+    st.write("Upload a csv for predictions")
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
         st.write(df.head())
-    #prediction
-    st.subheader('2. Predictions')
-    if st.button('Get Predictions'):
-        jsonreq = requests.post("http://api:3000/predict",json=df.to_dict())
+    # prediction
+    st.subheader("2. Predictions")
+    if st.button("Get Predictions"):
+        jsonreq = requests.post("http://api:3000/predict", json=df.to_dict())
         preds = jsonreq.json()
         df["Predictions"] = preds
         st.write(df)
-
-    
 
 
 if __name__ == "__main__":
